1cb08c798b76697723c4aa15b637f5e41d4f14e1
MohammedAref11 <mohammedaref.business11@gmail.com>
2026-07-16 20:19:24 +0400

    added dropdown menu

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log("it is working")
+const dropDownMenu = document.getElementById("menu"); 
+const unitsBtn = document.getElementById("unitsBtn"); 
+
+
+unitsBtn.addEventListener("click", () => { 
+    dropDownMenu.classList.toggle("opacity-0"); 
+    dropDownMenu.classList.toggle("opacity-100");
+    dropDownMenu.classList.toggle("invisible"); 
+    dropDownMenu.classList.toggle("visibile"); 
+    dropDownMenu.classList.toggle("-translate-y-4");
+    dropDownMenu.classList.toggle("-translate-y-0");
+    dropDownMenu.classList.toggle("pointer-events-none"); 
+})
+
+
+
+
